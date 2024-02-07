9acb9c49a490874812f11100a3f792fd9d46f8fe
LUZAmaxxp <ayman.allouch@e-polytechnique.ma>
2024-02-07 18:47:20 +0100

    feat : redirecting to a log in page

--- register/register.js
+++ register/register.js
@@ -1,33 +1,38 @@
-function checkin(emqil, password) {
+function checkin(email, password) {
   let text;
   let PasswordRegex = /^[A-Za-z]\w{7,14}$/;
   let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  if (emailRegex.test(emqil) && PasswordRegex.test(password)) {
+  if (emailRegex.test(email) && PasswordRegex.test(password)) {
     text = " Welcome !";
-    document.getElementById("demo").innerHTML = text;
+    document.getElementById("bemo").innerHTML = text;
+    window.location.href = "";
     return true;
   } else {
     text = "please enter a valid email and password";
-    document.getElementById("demo").innerHTML = text;
+    document.getElementById("bemo").innerHTML = text;
     return false;
   }
 }
 function addUser(event) {
   event.preventDefault();
-  let emqil = document.getElementById("numb").value;
+  let email = document.getElementById("numb").value;
   let password = document.getElementById("emb").value;
-  if (checkin(emqil, password)) {
+  if (checkin(email, password)) {
     let myuser = JSON.parse(localStorage.getItem("userData"));
-
     if (myuser === null) {
       let userData = [];
-      userData.push({ emqil, password });
+      userData.push({ email, password });
       localStorage.setItem("userData", JSON.stringify(userData));
     } else {
-      myuser.push({ emqil, password });
+      myuser.push({ email, password });
       localStorage.setItem("userdata", JSON.stringify(myuser));
     }
   } else {
     return "failed to add user";
   }
+}
+
+function redirect(event) {
+  event.preventDefault();
+  window.location.replace("../signin/signin.html");
 }
